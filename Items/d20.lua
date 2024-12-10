f92390ee6b84ae2b7e4abf1a51862b70a635419b
smg9000 <71861369+smg9000@users.noreply.github.com>
2024-12-10 11:08:34 -0600

    Update d20.lua

--- Items/d20.lua
+++ Items/d20.lua
@@ -6,20 +6,35 @@ local d20 = {
 	cost = 10,
 	order = 132,
 	atlas = "atlasd20",
-	config = {extra = {rollmult = 0}},
+	config = {extra = {rollmult = 0, multiply = 1}},
 	immutable = true,
 	no_dbl = true,
 	loc_vars = function(self, info_queue, center)
-		return { vars = { center.ability.extra.rollmult } }
+		return { vars = { center.ability.extra.rollmult, center.ability.extra.multiply } }
 	end,
 	calculate = function(self, card, context)
 		if context.joker_main and not context.before and not context.after then
 			--todo: check if duplicates of event are already started/finished
-			card.ability.extra.rollmult = roll_dice("cry_d20", 1, 20, {ignore_value = card.ability.extra.rollmult})
-			SMODS.Events["ev_cry_choco"..card.ability.extra.roll]:start()
-			return {
-				message = tostring(card.ability.extra.roll),
-				colour = G.C.GREEN
+			local rollnum = roll_dice("cry_d20", 20, {ignore_value = card.ability.extra.rollmult})
+			center.ability.extra.rollmult = rollnum.val
+			if rollnum.hit == true then
+				return {
+					message = localize({ type = "variable", key = "a_xmult", vars = { 2 * card.ability.extra.multiply } }),
+					Xmult_mod = card.ability.x_mult,
+					colour = G.C.MULT,
+				}
+			elseif rollnum.miss == true then
+				return {
+					message = localize({ type = "variable", key = "a_xmult", vars = { 0.5 / card.ability.extra.multiply } }),
+					Xmult_mod = card.ability.x_mult,
+					colour = G.C.MULT,
+				}
+			else 
+				return {
+					message = localize({ type = "variable", key = "a_mult", vars = { card.ability.extra.rollmult } }),
+					mult_mod = card.ability.extra.mult,
+					colour = G.C.MULT,
+				}
 			}
 		end
 	end,
