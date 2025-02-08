974e8f4171f4e98368cd14ffb2f75ee1218e8944
smg9000 <71861369+smg9000@users.noreply.github.com>
2025-02-08 13:02:29 -0600

    Update d20.lua

--- lib/d20.lua
+++ lib/d20.lua
@@ -3,23 +3,24 @@
 -- Currently this is very empty since D20 hasn't been fully implemented yet, but it should have a lot more later.
 
 --Will be moved to D20 file when that gets added
-function roll_dice(seed, max, config,)
-	local val
-	while not val or (config and config.ignore_value == val) do
-		val = pseudorandom(seed, 1, max)
-	end
-	local hit
-	local miss
-	if val => max then
-	    hit = true
-	elseif val <= 1 then
-	    miss = true
-	else hit = false miss = false
-	end
-	    
-	return {
-	    val,
-	    hit,
-	    miss,
-	    }
+function roll(dice, seed)
+    if type(dice.amount) == "table" then
+        return roll({
+            amount = roll(dice.amount, seed),
+            sides = dice.sides
+        })
+    else
+        if type(dice.sides) == "table" then
+            return roll({
+                amount = dice.amount,
+                sides = roll(dice.sides, seed)
+            })
+        else
+            local total = 0
+            for i = 1, dice.amount do
+                total = total + math.min(1 + math.floor(pseudorandom(seed) * (dice.sides)), dice.sides)
+            end
+            return total
+        end
+    end
 end
